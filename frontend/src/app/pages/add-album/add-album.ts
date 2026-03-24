fb7e4e4cf29aa0f00a0f4951be359c1b02414ed6
Joshua Powers <jpowerscts@gmail.com>
2026-03-23 21:49:08 -0400

    Initial Master commit

---
+++ frontend/src/app/pages/add-album/add-album.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-album',
+  imports: [],
+  templateUrl: './add-album.html',
+  styleUrl: './add-album.css',
+})
+export class AddAlbum {
+
+}
